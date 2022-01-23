5eb316632617e1b0cdb5c3867b7aaa847c417db3
Krissada Sarawit <topfeekrissada@outlook.com>
2022-01-23 10:32:08 +0700

    Update goldprice.py
    added LOAD_TOKEN() function

--- goldprice.py
+++ goldprice.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-TOKEN = ''
-with open('token.txt','r') as file:
-    TOKEN = file.read()
+def LOAD_TOKEN():
+    global TOKEN
+    with open('token.txt','r') as file:
+        TOKEN = file.read()
 
 def _lineNotify(payload, file=None):
     url = 'https://notify-api.line.me/api/notify'
@@ -16,19 +17,27 @@ def line_notify_message(message):
     payload = {'message': message}
     return _lineNotify(payload)
 
+def main():
+    LOAD_TOKEN()
+    # https://ทองคําราคา.com/tag/ราคาทอง-ฮั่วเซ่งเฮง/
+    url = "https://xn--42cah7d0cxcvbbb9x.com/tag/%E0%B8%A3%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%97%E0%B8%AD%E0%B8%87-%E0%B8%AE%E0%B8%B1%E0%B9%88%E0%B8%A7%E0%B9%80%E0%B8%8B%E0%B9%88%E0%B8%87%E0%B9%80%E0%B8%AE%E0%B8%87/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    status = response.status_code
 
-# https://ทองคําราคา.com/tag/ราคาทอง-ฮั่วเซ่งเฮง/
-url = "https://xn--42cah7d0cxcvbbb9x.com/tag/%E0%B8%A3%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%97%E0%B8%AD%E0%B8%87-%E0%B8%AE%E0%B8%B1%E0%B9%88%E0%B8%A7%E0%B9%80%E0%B8%8B%E0%B9%88%E0%B8%87%E0%B9%80%E0%B8%AE%E0%B8%87/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-status = response.status_code
+    data_price = soup.find_all("td", {"class": "em bg-em"})
+    ask_965 = data_price[2].text
+    bid_965 = data_price[3].text
+    update_date = soup.find_all("td", {"class": "em bg-span al-l txt-i"})[0].text
 
-data_price = soup.find_all("td", {"class": "em bg-em"})
-ask_965 = data_price[2].text
-bid_965 = data_price[3].text
-update_date = soup.find_all("td", {"class": "em bg-span al-l txt-i"})[0].text
+    if status == 200:
+        # print(bid_965,ask_965,update_date)
+        message = f"ทองแท่ง96.5%\nรับซื้อ : {bid_965}\nขายออก : {ask_965}\nอัพเดตเมื่อ :{update_date} \nฮั่วเซ่งเฮง"
+        line_notify_message(message)
+    else:
+        message = f"เกิดข้อผิดพลาด"
+        line_notify_message(message)
+        
 
-if status == 200:
-    # print(bid_965,ask_965,update_date)
-    goldprice_message = f"ทองแท่ง96.5%\nรับซื้อ : {bid_965}\nขายออก : {ask_965}\nอัพเดตเมื่อ :{update_date} \nฮั่วเซ่งเฮง"
-    line_notify_message(goldprice_message)
+if __name__ == "__main__":
+    main()
